10c2314a9e35710ad0a328b190544fa6233d4ac6
mohamedniyaz219 <mohamed.niyaz219@gmail.com>
2025-09-19 17:08:19 +0530

    updated resume link

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -117,7 +117,7 @@ export default function HeroSection() {
           </FadeInUp>
           <FadeInUp delay={950}>
             <InteractiveHoverButton
-              href="https://drive.google.com/file/d/1lG2rVEfW0l1N9ce0yyXJZqyEzft7Pfzi/view?usp=sharing"
+              href="https://drive.google.com/file/d/1swrM3DdHYX3GPnFjdjaTSmLYGJBCkMhi/view?usp=sharing"
               variant="outline"
               target="_blank"
               rel="noopener noreferrer"
